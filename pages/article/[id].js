79d789af5b67f9d40840cae681cb00bd0287990f
AyakaOnozuka131 <c_yu_again@icloud.com>
2021-11-05 07:48:15 +0900

    add: 一覧・詳細ページ追加

---
+++ pages/article/[id].js
@@ -0,0 +1,31 @@
+import { client } from "../../libs/client";
+
+export default function articleId({ article }){
+    return (
+        <main>
+            <h1>{article.title}</h1>
+            <p>{article.publishedAt}</p>
+            <div
+                dangerouslySetInnerHTML={{
+                __html: `${article.text}`,
+                }}
+            />
+        </main>
+    );
+}
+
+export const getStaticPaths = async() => {
+    const data = await client.get({ endpoint: 'article' });
+    const paths = data.contents.map((content)=>`/article/${content.id}`);
+    return { paths, fallback: false };
+}
+
+export const getStaticProps = async (context) => {
+    const id = context.params.id;
+    const data = await client.get({endpoint: 'article', contentId: id});
+    return {
+        props: {
+            article: data,
+        }
+    }
+}
