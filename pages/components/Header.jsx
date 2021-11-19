07c8ad79284dcaaa8b0dd3fcd3e7d28cb6ffa159
AyakaOnozuka131 <c_yu_again@icloud.com>
2021-11-19 21:11:17 +0900

    add : headerコンポーネント追加

---
+++ pages/components/Header.jsx
@@ -0,0 +1,13 @@
+import Link from 'next/link';
+
+export default function Header() {
+    return (
+        <header>
+            <h1>ねくすと・じぇいえす事始め!</h1>
+            <menu>Menu：
+                <Link href="/"><a>トップ</a></Link>｜
+                <Link href="/other"><a>別ページ</a></Link>
+            </menu>
+        </header>
+    );
+};
