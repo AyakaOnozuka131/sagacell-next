4ab032c5d5828acbcf1e6d052ae95fbb6d80f5e2
AyakaOnozuka131 <c_yu_again@icloud.com>
2021-11-24 21:52:14 +0900

    🚧 Add sass description to _app.js file

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
-import '../styles/globals.css'
+import '../styles/foundation/reset.scss'
+import '../styles/foundation/global.scss'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
