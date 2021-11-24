1d234b2b6084f0b7e64658e3fa6e6a19ce72e992
AyakaOnozuka131 <c_yu_again@icloud.com>
2021-11-24 23:17:57 +0900

    🚧 Add header component

--- pages/components/Header.jsx
+++ pages/components/Header.jsx
@@ -1,5 +1,8 @@
 import Link from 'next/link';
-import Layout from '../../styles/layout/layout.module.scss'
+import Layout from '../../styles/layout/layout.module.scss';
+import Component from '../../styles/component/component.module.scss';
+import Project from '../../styles/project/project.module.scss';
+import Utility from '../../styles/utility/utility.module.scss';
 
 export default function Header() {
     return (
