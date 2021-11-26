9680aeb9ce3c78a96714b6f43e77ae3fb033152c
AyakaOnozuka131 <c_yu_again@icloud.com>
2021-11-26 22:01:33 +0900

    🚧 Fixed the class name of layout component.

--- pages/components/Layout.jsx
+++ pages/components/Layout.jsx
@@ -1,12 +1,12 @@
-import Header from './components/Header';
-import Footer from './components/Footer';
+import Header from './Header';
+import Footer from './Footer';
 import LayoutClassName from '../../styles/layout/layout.module.scss';
 
 export default function Layout({children}) {
     return (
         <>
             <Header />
-            <main className={LayoutClassName['l-header']}>{children}</main>
+            <main className={LayoutClassName['l-main']}>{children}</main>
             <Footer />
         </>
     );
