b350234a4c1a8f26ed28b81363b049d6e5570537
AyakaOnozuka131 <c_yu_again@icloud.com>
2021-11-26 21:49:01 +0900

    🚧 Change the class name of CSS Modules

--- pages/components/Footer.jsx
+++ pages/components/Footer.jsx
@@ -1,52 +1,52 @@
 import Link from 'next/link';
 import Image from 'next/image'
-import Component from '../../styles/component/component.module.scss';
-import Project from '../../styles/project/project.module.scss';
+import ComponentClassName from '../../styles/component/component.module.scss';
+import ProjectClassName from '../../styles/project/project.module.scss';
 
 export default function Footer() {
     return (
-        <footer className={Project['p-footer']}>
-            <div className={Component['c-footer-nav']}>
-                <ul className={Component['c-footer-nav__list']}>
-                    <li className={Component['c-footer-nav__item']}>
+        <footer className={ProjectClassName['p-footer']}>
+            <div className={ComponentClassName['c-footer-nav']}>
+                <ul className={ComponentClassName['c-footer-nav__list']}>
+                    <li className={ComponentClassName['c-footer-nav__item']}>
                         <Link 
                             href="/" 
                             passHref 
                         >
-                            <a className={Component['c-footer-nav__link']}>TOP</a>
+                            <a className={ComponentClassName['c-footer-nav__link']}>TOP</a>
                         </Link>
                     </li>
-                    <li className={Component['c-footer-nav__item']}>
+                    <li className={ComponentClassName['c-footer-nav__item']}>
                         <Link 
                             href="/about" 
                             passHref 
                         >
-                            <a className={Component['c-footer-nav__link']}>このサイトについて</a>
+                            <a className={ComponentClassName['c-footer-nav__link']}>このサイトについて</a>
                         </Link>
                     </li>
-                    <li className={Component['c-footer-nav__item']}>
+                    <li className={ComponentClassName['c-footer-nav__item']}>
                         <Link 
                             href="/books" 
                             passHref 
                         >
-                            <a className={Component['c-footer-nav__link']}>おすすめ書籍</a>
+                            <a className={ComponentClassName['c-footer-nav__link']}>おすすめ書籍</a>
                         </Link>
                     </li>
-                    <li className={Component['c-footer-nav__item']}>
+                    <li className={ComponentClassName['c-footer-nav__item']}>
                         <Link 
                             href="/terms-and-privacy" 
                             passHref 
                         >
-                            <a className={Component['c-footer-nav__link']}>利用規約</a>
+                            <a className={ComponentClassName['c-footer-nav__link']}>利用規約</a>
                         </Link>
                     </li>
-                    <li className={Component['c-footer-nav__item']}>
-                        <a href="https://example.com/" target="_blank" rel="noopener noreferrer" className={Component['c-footer-nav__link']}>お問い合わせ</a>
+                    <li className={ComponentClassName['c-footer-nav__item']}>
+                        <a href="https://example.com/" target="_blank" rel="noopener noreferrer" className={ComponentClassName['c-footer-nav__link']}>お問い合わせ</a>
                     </li>
                 </ul>
             </div>
-            <div className={Project['p-footer__logo']}>
-                <div className={Project['p-footer__logo__img']}>
+            <div className={ProjectClassName['p-footer__logo']}>
+                <div className={ProjectClassName['p-footer__logo__img']}>
                     <Image 
                         src="/images/common/logo-white.svg" 
                         alt="SAGACELL：白塗りロゴ"
@@ -54,9 +54,9 @@ export default function Footer() {
                         height={19}
                     />
                 </div>
-                <span className={Project['p-footer__logo__catch-copy']}>探せる・見つかる・エクセル関数</span>
+                <span className={ProjectClassName['p-footer__logo__catch-copy']}>探せる・見つかる・エクセル関数</span>
             </div>
-            <small className={Component['c-footer__copy']}>Copyright &copy; 2021 SAGACELL All Rights Reserved.</small>
+            <small className={ComponentClassName['c-footer__copy']}>Copyright &copy; 2021 SAGACELL All Rights Reserved.</small>
         </footer>
     );
 };
--- pages/components/Header.jsx
+++ pages/components/Header.jsx
@@ -1,33 +1,33 @@
 import Link from 'next/link';
 import Image from 'next/image'
-import Layout from '../../styles/layout/layout.module.scss';
-import Component from '../../styles/component/component.module.scss';
-import Utility from '../../styles/utility/utility.module.scss';
+import LayoutClassName from '../../styles/layout/layout.module.scss';
+import ComponentClassName from '../../styles/component/component.module.scss';
+import UtilityClassName from '../../styles/utility/utility.module.scss';
 
 export default function Header() {
     return (
         <>
-            <div className={Component['c-overlay']}></div>
-            <header className={Component['c-header']}>
-                <div className={Layout['l-header']}>
+            <div className={ComponentClassName['c-overlay']}></div>
+            <header className={ComponentClassName['c-header']}>
+                <div className={LayoutClassName['l-header']}>
                     <button type="button" 
                         aria-controls="global-nav" 
                         aria-expanded="false" 
-                        className={`${Component['c-ham-button']} ${Component['c-hamburger']}`}
+                        className={`${ComponentClassName['c-ham-button']} ${ComponentClassName['c-hamburger']}`}
                     >
-                        <span className={Component['c-hamburger__line']}>
-                            <span className={Utility['u-visible-hidden']}>
+                        <span className={ComponentClassName['c-hamburger__line']}>
+                            <span className={UtilityClassName['u-visible-hidden']}>
                                 メニューを開閉する
                             </span>
                         </span>
                     </button>
-                    <div className={Layout['l-header__content']}>
-                    <div className={Component['c-header-logo']}>
+                    <div className={LayoutClassName['l-header__content']}>
+                    <div className={ComponentClassName['c-header-logo']}>
                         <Link 
                             href="/" 
                             passHref 
                         >
-                            <a className={Component['c-header-logo__link']}>
+                            <a className={ComponentClassName['c-header-logo__link']}>
                                 <Image 
                                     src="/images/common/logo.svg" 
                                     alt="ロゴ：match"
@@ -36,32 +36,32 @@ export default function Header() {
                                 />
                             </a>
                         </Link>
-                        <span className={Component['c-header-logo__tag-line']}>探せる・見つかる・エクセル関数</span>
+                        <span className={ComponentClassName['c-header-logo__tag-line']}>探せる・見つかる・エクセル関数</span>
                     </div>
-                    <nav className={Component['c-global-nav']} id="global-nav">
-                        <ul className={Component['c-global-nav__list']}>
-                            <li className={Component['c-global-nav__item']}>
+                    <nav className={ComponentClassName['c-global-nav']} id="global-nav">
+                        <ul className={ComponentClassName['c-global-nav__list']}>
+                            <li className={ComponentClassName['c-global-nav__item']}>
                                 <Link 
                                     href="/top" 
                                     passHref
                                 >
-                                    <a className={Component['c-global-nav__link']}>TOP</a>
+                                    <a className={ComponentClassName['c-global-nav__link']}>TOP</a>
                                 </Link>
                             </li>
-                            <li className={Component['c-global-nav__item']}>
+                            <li className={ComponentClassName['c-global-nav__item']}>
                                 <Link 
                                     href="/about" 
                                     passHref 
                                 >
-                                    <a className={Component['c-global-nav__link']}>このサイトについて</a>
+                                    <a className={ComponentClassName['c-global-nav__link']}>このサイトについて</a>
                                 </Link>
                             </li>
-                            <li className={Component['c-global-nav__item']}>
+                            <li className={ComponentClassName['c-global-nav__item']}>
                                 <Link 
                                     href="/books" 
                                     passHref 
                                 >
-                                    <a className={Component['c-global-nav__link']}>おすすめ書籍</a>
+                                    <a className={ComponentClassName['c-global-nav__link']}>おすすめ書籍</a>
                                 </Link>
                             </li>
                         </ul>
